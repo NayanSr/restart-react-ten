7ce8e0074567735eaa0de61c111d93d8c6dcd688
Nayan Suter <nayankbps@gmail.com>
2026-02-26 10:58:59 +0600

    adding normal class and conditional class

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,14 +1,17 @@
 import { use } from "react";
 import Country from "../Country/Country";
-
+import './Countries.css'
 const Countries = ({countriesPromise}) => {
     const data= use(countriesPromise);
 const countries= data.countries;
     return (
         <div>
             All Countries: {countries.length}
+            <div className="countries">
+
             {countries.map((country,index)=><Country key={index} country={country}/>) }
             {/* {countries.map(country=><Country key={country.name.official} country={country}/>) } */}
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,29 @@
-
+import { useState } from 'react';
+import './Country.css'
 const Country = ({country}) => {
-    const {name, flags, population}= country;
-    console.log(country)
+    const[visited, setVisited]=useState(false)
+    const {name, flags, population, area}= country;
+
+    // console.log(country)
+
+    const handleVisite=(country)=>{
+        console.log( country);
+        setVisited(!visited);
+    }
+
+
     return (
-        <div style={{"border":"2px solid green"}}>
+        <div className={`country ${visited && 'country-visited'}`}>
+        {/* <div style={{"border":"2px solid green"}}> */}
+        <div className='imgdiv'>
+
             <img src={flags.flags.png} alt={flags.flags.alt} />
+        </div>
             <h3>Name: {name.common} </h3>
             <p>Population : {population.population}</p>
+            <p>Area: {area.area}, {area.area>300000?"Big Country":"Small Country"}</p>
+            <p>{visited?"Visited":"Not Visited"}</p>
+            <button onClick={()=>handleVisite(name.common)}>{visited?"Visited":"Not Visited"}</button>
         </div>
     );
 };
